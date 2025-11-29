fa25724072333cdaaaec15bd76ad935b26699fbf
ualia946 <i946@inlumine.ual.es>
2025-11-29 12:02:46 +0100

    Cambio de region Azure Static Web App

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_static_web_app" "web_portfolio" {
   name = "stapp-ivelin-portafolio"
   resource_group_name = azurerm_resource_group.rg-webapp.name
-  location = azurerm_resource_group.rg-webapp.location
+  location = "westeurope"
 
   sku_size = "Free"
   sku_tier = "Free"
