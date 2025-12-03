eec8121569cb19080e6806f90cd5d6222ad211d0
ualia946 <i946@inlumine.ual.es>
2025-12-03 17:50:28 +0100

    Despliegue de cosmos DB para almacenar visitas de la página web

---
+++ terraform/cosmosdb.tf
@@ -0,0 +1,43 @@
+resource "azurerm_cosmosdb_account" "db_account" {
+  name = "cosmos-ivelin-portafolio-${random_string.suffix.result}"
+  location = azurerm_static_web_app.web_portfolio.location
+  resource_group_name = azurerm_resource_group.rg-webapp.name
+  offer_type = "Standard"
+  kind = "GlobalDocumentDB"
+
+  free_tier_enabled = "true"
+
+  consistency_policy {
+    consistency_level = "Session"
+  }
+
+  geo_location {
+    location = azurerm_static_web_app.web_portfolio.location
+    failover_priority = 0
+  }
+
+  tags = {
+    Environment = "Production"
+    Project = "Portfolio"
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "db_sql" {
+  name = "PortfolioDB"
+  account_name = azurerm_cosmosdb_account.db_account.name
+  resource_group_name = azurerm_resource_group.rg-webapp.name
+}
+
+resource "azurerm_cosmosdb_sql_container" "container_visitas" {
+  name = "Contador"
+  resource_group_name = azurerm_resource_group.rg-webapp.name
+  account_name = azurerm_cosmosdb_account.db_account.name
+  database_name = azurerm_cosmosdb_sql_database.db_sql.name
+  partition_key_paths = "/id"
+}
+
+resource "random_string" "suffix" {
+  length = 6
+  special = false
+  upper = false
+}
