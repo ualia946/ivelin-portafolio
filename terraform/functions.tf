0e49f90467832ebde2319fe099e3ab52d83f2ee3
ualia946 <i946@inlumine.ual.es>
2025-12-08 17:50:49 +0100

    URL de Azure Static Web App (portfolio) establecida en la propiedad CORS de Azure Function

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -37,7 +37,7 @@ resource "azurerm_linux_function_app" "function-app" {
       }
 
       cors {
-        allowed_origins = ["www.ivelinapostolov.com"]
+        allowed_origins = [azurerm_static_web_app.web_portfolio.default_host_name]
       }
     }
 
