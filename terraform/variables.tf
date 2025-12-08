5f5dd6aae0062bf23e7e8525979c1ecce3371080
ualia946 <i946@inlumine.ual.es>
2025-12-08 13:18:21 +0100

    Correción de error de sintaxis

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "cosmos_db_endpoint" {
   description = "URL de la base de datos"
-  type = "string"
+  type = string
   sensitive = true
 }
 
  variable "cosmos_db_key" {
    description = "Clave de la base de datos"
-   type = "string"
+   type = string
    sensitive = true
  }
