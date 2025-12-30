b8c891d3ec6421fe361c865c9fcaa49cd82b83eb
ualia946 <i946@inlumine.ual.es>
2025-12-30 08:37:15 +0100

    Add Azure icons and InfraestructureDiagram component - Added new SVG icons for various Azure services including Virtual Networks, Network Security Groups, Public IP Addresses, Network Interfaces, Storage Accounts, Azure Cosmos DB, Key Vaults, and more. - Implemented the InfraestructureDiagram component to visualize infrastructure using React Flow. - Integrated loading state with a spinner while fetching graph elements from the API. - Created a new Astro page to render the InfraestructureDiagram within a layout.

---
+++ api/src/functions/obtenerInfraestructura.js
@@ -0,0 +1,181 @@
+const { app } = require('@azure/functions');
+const { ResourceGraphClient } = require("@azure/arm-resourcegraph");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+const RESOURCE_MAP = {
+    // Compute & Web
+    "microsoft.web/sites": "App Service / Function",
+    "microsoft.web/static-sites": "Static Web App",
+    "microsoft.web/staticsites": "Static Web App", // Azure a veces usa este alias
+    "microsoft.web/serverfarms": "App Service Plan",
+    "microsoft.compute/virtualmachines": "Virtual Machine",
+    
+    // Databases
+    "microsoft.documentdb/databaseaccounts": "Azure Cosmos DB",
+    "microsoft.documentdb/mongoclusters": "Cosmos DB for MongoDB (vCore)",
+    
+    // Storage
+    "microsoft.storage/storageaccounts": "Storage Account",
+    
+    // Monitoring & Insights 
+    "microsoft.insights/components": "Application Insights",
+    "microsoft.insights/actiongroups": "Action Groups",
+    "microsoft.alertsmanagement/smartdetectoralertrules": "Smart Detector Alert Rules",
+    "microsoft.insights/metricalerts": "Metric Alerts",
+    
+    // Networking & Security
+    "microsoft.keyvault/vaults": "Key Vault",
+    "microsoft.network/virtualnetworks": "Virtual Network",
+    "microsoft.network/networkinterfaces": "Network Interface",
+    "microsoft.network/publicipaddresses": "Public IP Address",
+    "microsoft.network/networksecuritygroups": "Network Security Group"
+};
+
+
+function createGraph(nodes, edges, rawResources){
+    nodes.push({
+        id: "rg-webapp",
+        position: {x:0, y:0},
+        data: {
+            label: "rg-webapp",
+            name: "rg-webapp",
+            type: "Resource Group",
+            location: "Spain Central"
+        }
+    })
+
+    //const nodeNames = new Set()
+
+    for(const rawResource of rawResources){
+        const resource = changeType(rawResource)
+        //if (nodeNames.has(resource.name)) continue
+        //nodeNames.add(resource.name)
+        addNode(nodes, resource)
+    }
+    addEdges(edges, nodes, rawResources)    
+}
+
+function changeType(resource){
+    const originalType = resource.type
+    let friendlyType = RESOURCE_MAP[originalType]
+
+    if(!friendlyType){
+        const parts = originalType.split("/")
+        const rawType = parts[parts.length - 1]
+        friendlyType = rawType.charAt(0).toUpperCase() + rawType.slice(1)
+    }
+
+    return {
+        name: resource.name,
+        type: friendlyType,
+        location: resource.location
+    }
+}
+
+function addNode(nodes, resource){
+    nodes.push({
+        id: resource.name,
+        position: {x:0, y:0},
+        data: {
+            name: resource.name,
+            type: resource.type,
+            location: resource.location
+        }
+    })
+}
+
+function addEdges(edges, nodes, rawResource){
+    const resourceGroupId = nodes[0].id
+    for(const node of nodes){
+        if (node.id == "rg-webapp") continue
+        edges.push({
+            id: `${resourceGroupId}_to_${node.id}`,
+            source: resourceGroupId,
+            target: node.id,
+            animated: true,
+            label: "Contiene",
+            style: { stroke: "#69c7c7" }
+        })
+    }
+
+    const functionAppNode = nodes.find(n => n.data.type.includes("App Service / Function"))
+    if(functionAppNode){
+        const functionResource = rawResource.find(r => r.name === functionAppNode.id)
+        console.log(functionResource.properties.serverFarmId)
+        if(functionResource && functionResource.properties.serverFarmId){
+            const targetResourceName = functionResource.properties.serverFarmId.split("/").pop()
+            const targetResourceNode = nodes.find(n => n.id === targetResourceName)
+            if(targetResourceNode){
+                edges.push({
+                    id: `${functionAppNode.id}_to_${targetResourceNode.id}`,
+                    source: functionAppNode.id,
+                    target: targetResourceNode.id,
+                    animated: true,
+                    label: "Actualizar visitas",
+                    style: { stroke: "#7836cf" }
+                })
+            }
+        }
+
+        const dbNodes = nodes.filter(n => n.data.type.includes("DB"))
+        for(const dbNode of dbNodes){
+            edges.push({
+                id: `${functionAppNode.id}_to_${dbNode.id}`,
+                source: functionAppNode.id,
+                target: dbNode.id,
+                animated: true,
+                label: "Lee/Escribe",
+                style: { stroke: "#7836cf" }
+            });
+        }
+
+        const storageNodes = nodes.filter(n => n.data.type.includes("Storage"))
+        for (const storageNode of storageNodes){
+            edges.push({
+                id: `${functionAppNode.id}_to_${storageNode.id}`,
+                source: functionAppNode.id,
+                target: storageNode.id,
+                animated: true,
+                label: "Registros/Código",
+                style: { stroke: "#7836cf" }
+            });
+        }
+
+    }
+}
+
+
+app.http('obtenerInfraestructura', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        try{    
+            const credential = new DefaultAzureCredential();
+            const client = new ResourceGraphClient(credential)
+
+            const query = `
+                resources
+                | where resourceGroup contains "rg-webapp"
+                | project name, type, location, properties
+                | order by name asc 
+            `
+            const result = await client.resources({
+                query: query
+            })
+
+            const nodes = []
+            const edges = []
+            
+            createGraph(nodes, edges, result.data)
+            console.log(nodes)
+            console.log(edges)
+
+            return{
+                body: JSON.stringify({nodes: nodes, edges: edges})
+            }
+
+        }catch (error){
+            console.error(error)
+        }
+    }
+});
---
+++ webapp/src/components/infraestructure/InfraestructureDiagram.jsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from 'react'
+import { ReactFlow, Controls, Background } from '@xyflow/react'
+import '@xyflow/react/dist/style.css'
+import dagre from 'dagre';
+import {  BounceLoader } from 'react-spinners'
+
+
+const RESOURCE_IMAGES = {
+    "Resource Group": "icons/azure-icons/Resource-Groups.svg",
+    "App Service / Function": "/icons/azure-icons/10029-icon-service-Function-Apps.svg",
+    "Static Web App": "/icons/azure-icons/01007-icon-service-Static-Apps.svg",
+    "App Service Plan": "/icons/azure-icons/00046-icon-service-App-Service-Plans.svg",
+    "Virtual Machine": "/icons/azure-icons/10021-icon-service-Virtual-Machine.svg",
+    "Azure Cosmos DB": "/icons/azure-icons/10121-icon-service-Azure-Cosmos-DB.svg",
+    "Cosmos DB for MongoDB (vCore)": "/icons/azure-icons/documentdb.svg",
+    "Storage Account": "/icons/azure-icons/10087-icon-service-Storage-Accounts-(Classic).svg",
+    "Application Insights": "/icons/azure-icons/00012-icon-service-Application-Insights.svg",
+    "Action Groups": "/icons/azure-icons/00002-icon-service-Alerts.svg",
+    "Smart Detector Alert Rules": "/icons/azure-icons/00002-icon-service-Alerts.svg",
+    "Metric Alerts": "/icons/azure-icons/00020-icon-service-Metrics.svg",
+    "Key Vault": "/icons/azure-icons/10245-icon-service-Key-Vaults.svg",
+    "Virtual Network": "/icons/azure-icons/10061-icon-service-Virtual-Networks.svg",
+    "Network Interface": "/icons/azure-icons/10080-icon-service-Network-Interfaces.svg",
+    "Public IP Address": "/icons/azure-icons/10069-icon-service-Public-IP-Addresses.svg",
+    "Network Security Group": "/icons/azure-icons/10067-icon-service-Network-Security-Groups.svg"
+}
+
+const dagreGraph = new dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+const nodeWidth = 200;
+const nodeHeight = 130;
+
+const getLayoutedElements = (nodes, edges, direction = 'TB') => {
+  const isHorizontal = direction === 'LR';
+  dagreGraph.setGraph({ rankdir: direction });
+
+  nodes.forEach((node) => {
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+  });
+ 
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
+
+  dagre.layout(dagreGraph);
+
+  const layoutedNodes = nodes.map((node) => {
+    const nodeWithPosition = dagreGraph.node(node.id);
+    return {
+      ...node,
+      targetPosition: isHorizontal ? 'left' : 'top',
+      sourcePosition: isHorizontal ? 'right' : 'bottom',
+      position: {
+        x: nodeWithPosition.x - nodeWidth / 2,
+        y: nodeWithPosition.y - nodeHeight / 2,
+      },
+      data:{
+        ...node.data,
+        label: (
+            <div className="flex flex-col items-center justify-center">
+                <img src={RESOURCE_IMAGES[node.data.type]} alt="Imagen" className=' w-8'/>
+                <span className="font-bold text-gray-900 text-sm mb-1">
+                     {node.data.name}
+                </span>
+                <span className="text-gray-800 text-[10px] uppercase tracking-wide">
+                    {node.data.type}
+                </span>
+            </div>
+        )
+      }
+    };
+  });
+
+  return { nodes: layoutedNodes, edges };
+};
+
+
+
+export default function InfraestructureDiagram(){
+
+    const [nodes, setNodes] = useState([])
+    const [edges, setEdges] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        async function getGraphElements(){
+            try{
+                const response = await fetch(import.meta.env.PUBLIC_API_INFRAESTRUCTURE_URL)
+                const data = await response.json()
+                const nodes = data.nodes
+                const edges = data.edges
+
+                const {nodes: layoutedNodes, edges: layoutedEdges} = getLayoutedElements(nodes,edges)
+                setNodes(layoutedNodes)
+                setEdges(layoutedEdges)
+            }catch (error){
+                console.error(error)
+            }finally {
+                setIsLoading(false)
+            }
+        }
+
+        getGraphElements()
+    }, [])
+
+    return(
+        <div className='w-full h-[800px] flex justify-center items-center'>
+            {isLoading ? (<BounceLoader color='#57fff9' size={200} speedMultiplier={2} />) : (<ReactFlow
+                nodes={nodes}
+                edges={edges}
+                fitView
+                className=' text-black'
+            >
+                <Background color="#4a5568" gap={16}/>
+                <Controls className=' border-2 border-[#e2e8f0] border-pu'/>
+            </ReactFlow>)}
+               
+        </div>
+    )
+}
