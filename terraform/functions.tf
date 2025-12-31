4ec2a35b0b538c98a89d2b36989a0c62a4599484
ualia946 <i946@inlumine.ual.es>
2025-12-31 11:58:25 +0100

    Add depends_on attribute to role assignment for function app dependency

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -58,4 +58,5 @@ resource "azurerm_role_assignment" "storage_access" {
   scope                = azurerm_storage_account.storage.id
   role_definition_name = "Storage Blob Data Owner"
   principal_id         = azurerm_linux_function_app_flex_consumption.function-app.identity[0].principal_id
+  depends_on = [ azurerm_linux_function_app.function-app ]
 }
