0cf2c7e8a1fc8ed1ae47e6eb6dc15f5a68d315b0
ualia946 <i946@inlumine.ual.es>
2025-12-31 10:32:02 +0100

    Removed index.js

--- api/index.js
+++
@@ -1,6 +0,0 @@
-// Bootstrap file for Azure Functions (Node.js v4 programming model)
-// Explicitly load all function modules so they can register with the runtime via app.http(...)
-
-require('./src/functions/actualizarVisitas');
-require('./src/functions/obtenerInfraestructura');
-require('./src/functions/obtenerRespuestaChatBot');
