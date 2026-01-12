b770f2e18a0a71ee4f5292af93ea8639328aa080
ualia946 <i946@inlumine.ual.es>
2026-01-12 19:32:18 +0100

    fix: fetch and display current location details including flag, country, and city

--- webapp/src/components/infraestructure/Globe3D.jsx
+++ webapp/src/components/infraestructure/Globe3D.jsx
@@ -12,6 +12,9 @@ export default function Globe3D() {
     const [arcs, setArcs] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const [currentIp, setCurrentIp] = useState("")
+    const [currentFlagCountry, setCurrentFlagCountry] = useState()
+    const [countryName, setCountryName] = useState("")
+    const [cityName, setCityName] = useState("")
 
     useEffect(() => {
         (async () => {
@@ -38,6 +41,9 @@ export default function Globe3D() {
                 const response = await fetch("https://ipwho.is/")
                 const data = await response.json()
                 setCurrentIp(data.ip)
+                setCurrentFlagCountry(data.flag.emoji)
+                setCountryName(data.country)
+                setCityName(data.city)
             } catch (error) {
                 console.error("Erro al obtener la ip: ", error)
             }
@@ -50,49 +56,73 @@ export default function Globe3D() {
     useEffect(() => {
         if(globeEl.current){
             globeEl.current.controls().autoRotate = true
-            globeEl.current.controls().autoRotateSpeed = 0.5
+            globeEl.current.controls().autoRotateSpeed = 0
         }
     }, [isLoading])
 
 
     return(
-        <div className=' w-full h-[800px] flex justify-center items-center pt-20'>
-            {isLoading ?
-                <BounceLoader 
-                    color='#57fff9' 
-                    size={200} 
-                    speedMultiplier={2} 
-                /> 
-                : 
-                <Globe
-                    ref={globeEl}
-            
-                    // --- APARIENCIA GLOBAL ---
-                    globeImageUrl="https://unpkg.com/three-globe/example/img/earth-night.jpg"
-                    backgroundColor="rgba(0,0,0,0)" // Transparente
-                    atmosphereColor="#69c7c7" // Tu color corporativo (cian)
-                    atmosphereAltitude={0.15}
-
-                    // --- CAPA DE PUNTOS (Tus usuarios) ---
-                    pointsData={points}
-                    pointLat="latitude"   
-                    pointLng="longitude"
-                    pointColor={(p) => (currentIp === p.id ? "#7836cf" : "#69c7c7")} 
-                    pointAltitude={0.02}
-                    pointRadius={0.2}
-
-                    // --- CAPA DE ARCOS (Las conexiones) ---
-                    arcsData={arcs}
-                    arcColor="color"
-                    arcDashLength={0.4}        // Longitud del "rayo" láser
-                    arcDashGap={2}            
-                    arcDashAnimateTime={1500}  
-                    // arcStroke={null}        // Descomenta esto si quieres que solo se vean los "rayos" y no la línea base
-
-                    // --- TAMAÑO ---
+        <>
+            <div className=' w-full h-[840px] lg:h-[890px] flex justify-center items-center p-20'>
+                {isLoading ?
+                    <BounceLoader 
+                        color='#57fff9' 
+                        size={200} 
+                        speedMultiplier={2} 
+                    /> 
+                    : 
+                    <Globe
+                        ref={globeEl}
                 
-                />
-            }
-        </div>
+                        // --- APARIENCIA GLOBAL ---
+                        globeImageUrl="https://unpkg.com/three-globe/example/img/earth-night.jpg"
+                        backgroundColor="rgba(0,0,0,0)" // Transparente
+                        atmosphereColor="#69c7c7" // Tu color corporativo (cian)
+                        atmosphereAltitude={0.15}
+
+                        // --- CAPA DE PUNTOS (Tus usuarios) ---
+                        pointsData={points}
+                        pointLat="latitude"   
+                        pointLng="longitude"
+                        pointColor={(p) => (currentIp === p.id ? "#7836cf" : "#69c7c7")} 
+                        pointAltitude={0.02}
+                        pointRadius={0.1}
+
+                        // --- CAPA DE ARCOS (Las conexiones) ---
+                        arcsData={arcs}
+                        arcColor="color"
+                        arcDashLength={0.4}        // Longitud del "rayo" láser
+                        arcDashGap={2}            
+                        arcDashAnimateTime={1500}  
+                        // arcStroke={null}        // Descomenta esto si quieres que solo se vean los "rayos" y no la línea base
+
+                        // --- TAMAÑO ---
+                    
+                    />
+                }
+            </div>
+
+            {!isLoading && <div className=' 
+                z-50
+                fixed
+                bottom-4 sm:bottom-6 md:bottom-10
+                left-0 sm:left-6 md:left-10
+                pointer-events-none
+                bg-white/10
+                border border-white/20
+                backdrop-blur-sm
+                shadow-lg
+                rounded-md
+                px-3
+                py-1 
+                sm: text-sm
+                md: text-[1rem]
+                ml-5
+                transition-opacity
+                animated-pulse '
+            >
+                {`${currentFlagCountry} ${countryName}, ${cityName}`}
+            </div>}
+        </>
     )
 }
